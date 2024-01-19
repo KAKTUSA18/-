7b12d182dfec77ffb591fdb4a503fd388127984d
KAKTUSA18 <146727712+KAKTUSA18@users.noreply.github.com>
2024-01-19 14:55:53 +0300

    Add files via upload

---
+++ QQ.xaml.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Логика взаимодействия для QQ.xaml
+    /// </summary>
+    public partial class QQ : Window
+    {
+        public QQ()
+        {
+            InitializeComponent();
+        }
+
+        private void save_Click(object sender, RoutedEventArgs e)
+        {
+            string name;
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Filter = "Image files (*.BMP, *.JPG, *.GIF, *.TIF, *.PNG, *.ICO, *.EMF, *.WMF)| *.bmp; *.jpg; *.gif; *.tif; *.png; *.ico; *.emf; *.wmf";
+
+            if (openDialog.ShowDialog() == true)
+            {
+                Im.Source = new BitmapImage(new Uri(openDialog.FileName));
+                name = openDialog.FileName;
+            }
+
+            SqlConnection sqlConnection = new SqlConnection();
+            string connectionString = "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = faizullaev; Integrated Security = True; Connect Timeout = 30; Encrypt = False; Trust Server Certificate = False; Application Intent = ReadWrite; Multi Subnet Failover = False";
+            string query = "SELECT * FROM Users";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                string value = reader.GetString(1);
+                                nameTB.Text = value;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+
+    }
+}
+    
+
